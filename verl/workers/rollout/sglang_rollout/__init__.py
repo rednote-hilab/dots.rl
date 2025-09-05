dd9b5dce1cf7477cf604de1e0c5b8a275ff237b1
ziqi-wlb <550461053@qq.com>
2025-09-05 11:06:14 +0000

    Porting: support dots model register for engine

--- verl/workers/rollout/sglang_rollout/__init__.py
+++ verl/workers/rollout/sglang_rollout/__init__.py
@@ -10,3 +10,25 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+
+import multiprocessing as mp
+from sglang.srt.entrypoints.engine import mp as sglang_mp
+from .sglang_rollout import SGLangRollout
+
+
+class PatchedProcess(mp.Process):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        self.registe_dots_model()
+        super().run()
+
+    def registe_dots_model(self):
+        from verl.models.dots.sglang import XdgMoEForCausalLM
+        from sglang.srt.models.registry import ModelRegistry
+        ModelRegistry.models["XdgMoEForCausalLM"] = XdgMoEForCausalLM
+
+sglang_mp.Process = PatchedProcess
+
+__all__ = ["SGLangRollout"]
